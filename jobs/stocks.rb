e46aa5b6bf9eb19906685cfd0b1bc73ca2f6585a
Ryan Bradford <ryanbradford333@gmail.com>
2018-12-23 10:34:46 -0500

    Started the stock

--- jobs/stocks.rb
+++ jobs/stocks.rb
@@ -3,7 +3,7 @@ require 'dotenv/load'
 require 'json'
  
 SCHEDULER.every '3600s', :first_in => 0 do |job|
-    uri = URI('https://api.iextrading.com/1.0/stock/SPY/batch?types=quote,chart&range=1m&last=10')
+    uri = URI('https://api.iextrading.com/1.0/stock/SPY/batch?types=quote')#,chart&range=1m&last=10')
     Net::HTTP.start(uri.host, uri.port,
     :use_ssl => uri.scheme == 'https') do |http|
     request = Net::HTTP::Get.new uri
@@ -12,10 +12,13 @@ SCHEDULER.every '3600s', :first_in => 0 do |job|
     json = JSON.parse(response.body)
     quote = json['quote']
     isClosed = quote['latestSource'].eql? 'Close'
+    prevClose = quote['previousClose']
+    curClose = quote['latestPrice']
+    change = (((curClose - prevClose) / prevClose)*100)
+    change = change.round(2)
     send_event('stock', { :company => quote['companyName'],
-                          :prevClose => quote['previousClose'],
-                         :isOpen => isClosed,
-                        :value => quote['latestPrice']})
+                          :change => "#{change}%",
+                         :isOpen => isClosed})
     end
 end
 
