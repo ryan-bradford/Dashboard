39d911ca7ff4ad5ab45b02a29eedc2f98f7669bc
Ryan Bradford <ryanbradford333@gmail.com>
2018-12-28 13:00:05 -0500

    Fixed delete tasks bug.

--- jobs/todo.rb
+++ jobs/todo.rb
@@ -8,11 +8,13 @@ SCHEDULER.every '20s', :first_in => 0 do |job|
     items = @client.sync_items.collection
     todo = []
     items.each do |x|
-        date =  Date.parse x[1].date_string
-        date_completed = x[1].date_completed
-        is_deleted = x[1].is_deleted
-        if date == Date.today && !(date_completed.instance_of? String) && is_deleted == 0
-            todo.push({:content => x[1].content})
+        if (x[1].date_string.instance_of? String)
+            date =  Date.parse x[1].date_string
+            date_completed = x[1].date_completed
+            is_deleted = x[1].is_deleted
+            if date == Date.today && !(date_completed.instance_of? String) && is_deleted == 0
+                todo.push({:content => x[1].content})
+            end
         end
     end
     send_event('todo', {:title => "Todo", :items => todo})
